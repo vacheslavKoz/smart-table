1d1efa9b69dce1e18b67ed38b3d1edef851f2d36
slava <vaceslavkozakevic84@gmail.com>
2026-03-16 00:04:20 +0300

    16 марта 0000

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -2,10 +2,10 @@ import {sortCollection, sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
     return (data, state, action) => {
-      //  let field = null;
-      // let order = null;
-       let field = "total" ;
-       let order = "up";
+       let field = null;
+       let order = null;
+       //let field = "total" ;
+       //let order = "up";
         if (action && action.name === 'sort') {
             // @todo: #3.1 — запомнить выбранный режим сортировки
 action.dataset.value = sortMap[action.dataset.value];    // Сохраним и применим как текущее следующее состояние из карты
